576e37fe945f3e841016b94530608b240a72f90c
Karlamon <zbv9522@aut.ac.nz>
2015-05-28 20:17:17 +1200

    Update about.php

--- about.php
+++ about.php
@@ -10,9 +10,9 @@
     <h1>Article Searcher</h1>
     <h2>About this assignment</h2>
     <p>Assignment members:</p>
-    <li>Liang Chen ()</li>
-    <li>Qingyang Guo (1395160)</li>
-    <li>Karl Smith (1390533)</li>
+    <li>Liang Chen (1392702) - <a href="mailto:cl19940413@hotmail.com">cl19940413@hotmail.com</a></li>
+    <li>Qingyang Guo (1395160) - <a href="mailto:g594xz@163.com">g594xz@163.com</a></li>
+    <li>Karl Smith (1390533) - <a href="mailto:zbv9522@aut.ac.nz">zbv9522@aut.ac.nz</a></li>
     <p><a href="index.php">Return to Home Page</a></p>
 </body>
 </html>
