a9d683d32db770c46173f4a0c0c56470ec5ab094
lolcatchuplol <cl19940413@hotmail.com>
2015-05-29 10:19:14 +1200

    Update searcharticleform.php

--- searcharticleform.php
+++ searcharticleform.php
@@ -11,7 +11,13 @@
     <h2>Search article</h2>
     <form action="searcharticleprocess.php" method="get">
         <p align="left"><label><b>Title: </b>
-        <input type="text" name="title" maxlength="100" size="100"</label><br>
+        <input type="text" name="title" maxlength="100" style="width:90%;"</label><br><br>
+		<b>author: </b>
+		<input type="text" name="author" maxlength="30" size="30"</label><br><br>
+		<b>method: </b>
+		<input type="text" name="method" maxlength="20" size="20"</label><br><br>
+		<b>venue: </b>
+		<input type="text" name="venue" maxlength="10" size="10"</label><br><br>
         <input type="submit" value="Search"></p>
     </form>
 	<p><a href="index.php">Return to Home Page</a></p>
