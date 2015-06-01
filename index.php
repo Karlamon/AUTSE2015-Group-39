160a78cf06d220b68a3789d8459dc6582e0580aa
lolcatchuplol <cl19940413@hotmail.com>
2015-06-02 02:45:23 +1200

    Update index.php

--- index.php
+++ index.php
@@ -1,17 +1,25 @@
 <!--Karl Smith (1390533)-->
 <!DOCTYPE html>
 <html>
-<head>
-	<meta http-equiv="content-type" content="text/html; charset=utf-8" />
-    <link rel="stylesheet" type="text/css" href="style.css">
-	<title>Status Posting System</title>
-</head>
-<body>
-	<h1>Article Searcher</h1>
-    <h2>Home Page</h2>
-    <p><a href="loginform.php">Submit new article</a><br>
-        <a href="searcharticleform.php">Search article</a><br>
-        <a href="createaccountform.php">Create account</a><br>
-        <a href="about.php">About this assignment</a></p>
+    <head>
+        <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+        <link rel="stylesheet" type="text/css" href="style.css">
+        <title>Article Searcher System</title>
+    </head>
+    <body>
+        <header>
+            <h1>Article Searcher</h1>
+        </header>
+    <bar>
+        <div class="container">
+            <div id="links">
+                <li class="selected"><a href="index.php">Home Page</a></br></li>
+                <li><a href="loginform.php">Submit new article</a><br></li>
+                <li><a href="searcharticleform.php">Search article</a><br></li>
+                <li><a href="createaccountform.php">Create account</a><br></li>
+                <li><a href="about.php">About this assignment</a></li>
+            </div>
+        </div>
+    </bar>
 </body>
 </html>
