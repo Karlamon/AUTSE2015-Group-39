d0ad6a065fa1e99aef30128aed2562be2ae3d038
Karlamon <zbv9522@aut.ac.nz>
2015-06-01 15:25:05 +1200

    Create loginform.php

---
+++ loginform.php
@@ -0,0 +1,23 @@
+<!--Karl Smith (1390533)-->
+<!DOCTYPE html>
+<html>
+<head>
+	<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+    <link rel="stylesheet" type="text/css" href="style.css">
+	<title>Submit new article - Article Searcher</title>
+</head>
+<body>
+	<h1>Article Searcher</h1>
+    <h2>Login</h2>
+    All fields required.
+	<form action="loginprocess.php" method="get">
+        <p align="left"><label><b>Username: </b>
+            <input type="text" name="username" maxlength="30" size="30"</label></p>
+        <p align="left"><label><b>Password: </b>
+            <input type="text" name="password" maxlength="30" size="30"</label></p>
+        <p><input type="submit" value="Login"></p>
+	</form>
+    <p><a href="createaccountform.php">Create account</a><br>
+    <a href="index.php">Return to Home Page</a></p>
+</body>
+</html>
