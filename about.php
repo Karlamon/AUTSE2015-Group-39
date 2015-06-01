92bc7963b6c40acb027f4bbb602e0c85a738cf2f
lolcatchuplol <cl19940413@hotmail.com>
2015-06-02 02:42:33 +1200

    Update about.php

--- about.php
+++ about.php
@@ -1,18 +1,29 @@
 <!--Karl Smith (1390533)-->
 <!DOCTYPE html>
 <html>
-<head>
-	<meta http-equiv="content-type" content="text/html; charset=utf-8" />
-    <link rel="stylesheet" type="text/css" href="style.css">
-	<title>About this assignment - Article Searcher</title>
-</head>
-<body>
-    <h1>Article Searcher</h1>
-    <h2>About this assignment</h2>
-    <p>Assignment members:</p>
-    <li>Liang Chen (1392702) - <a href="mailto:cl19940413@hotmail.com">cl19940413@hotmail.com</a></li>
-    <li>Qingyang Guo (1395160) - <a href="mailto:g594xz@163.com">g594xz@163.com</a></li>
-    <li>Karl Smith (1390533) - <a href="mailto:zbv9522@aut.ac.nz">zbv9522@aut.ac.nz</a></li>
-    <p><a href="index.php">Return to Home Page</a></p>
+    <head>
+        <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+        <link rel="stylesheet" type="text/css" href="style.css">
+        <title>About this assignment - Article Searcher</title>
+    </head>
+    <body>
+        <header>
+            <h1>Article Searcher</h1>
+        </header>
+    <bar>
+        <div class="container">
+            <div id="links">
+                <li><a href="index.php">Home Page</a></li>
+                <li class="selected"><a href="about.php">About this assignment</a></br></li>
+            </div>
+        </div>
+    </bar>
+    <div id="text">
+        <h2>About this assignment</h2>
+        <p>Assignment members:</p>
+        <li>Liang Chen (1392702) - <a href="mailto:cl19940413@hotmail.com">cl19940413@hotmail.com</a></li>
+        <li>Qingyang Guo (1395160) - <a href="mailto:g594xz@163.com">g594xz@163.com</a></li>
+        <li>Karl Smith (1390533) - <a href="mailto:zbv9522@aut.ac.nz">zbv9522@aut.ac.nz</a></li>
+    </div>
 </body>
 </html>
