9d08aaf83b5491ef3c38fbca9b49d1e11f4e02b4
lolcatchuplol <cl19940413@hotmail.com>
2015-06-02 02:45:37 +1200

    Update loginform.php

--- loginform.php
+++ loginform.php
@@ -1,23 +1,38 @@
 <!--Karl Smith (1390533)-->
 <!DOCTYPE html>
 <html>
-<head>
-	<meta http-equiv="content-type" content="text/html; charset=utf-8" />
-    <link rel="stylesheet" type="text/css" href="style.css">
-	<title>Login - Article Searcher</title>
-</head>
-<body>
-	<h1>Article Searcher</h1>
-    <h2>Login</h2>
-    You must log in to submit a new article.
-	<form action="loginprocess.php" method="get">
-        <p align="left"><label><b>Username: </b>
-            <input type="text" name="username" maxlength="30" size="30"</label></p>
-        <p align="left"><label><b>Password: </b>
-            <input type="text" name="password" maxlength="30" size="30"</label></p>
-        <p><input type="submit" value="Login"></p>
-	</form>
-    <p><a href="createaccountform.php">Create account</a><br>
-    <a href="index.php">Return to Home Page</a></p>
+    <head>
+        <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+        <link rel="stylesheet" type="text/css" href="style.css">
+        <title>Login - Article Searcher</title>
+    </head>
+    <body>
+        <header>
+            <h1>Article Searcher</h1>
+        </header>
+    <bar>
+        <div class="container">
+            <div id="links">
+				<li><a href="index.php">Home Page</a></li>
+                <li class="selected"><a href="loginform.php">Login</a></br></li>
+                <li><a href="createaccountform.php">Create account</a><br></li>
+            </div>
+        </div>
+    </bar>
+    <div id="text">
+        <h2>Login</h2>
+        *You must log in to submit a new article.
+        <form action="loginprocess.php" method="get">
+            <div class="row">
+                <label class="col-15">Username</label>
+                <input type="text" name="username" maxlength="30" size="20"</label>
+            </div>
+            <div class="row">
+                <label class="col-15">Password</label>
+                <input type="text" name="password" maxlength="30" size="20"</label>
+            </div>
+            <p><input type="submit" value="Login"></p>
+        </form>
+    </div>
 </body>
 </html>
