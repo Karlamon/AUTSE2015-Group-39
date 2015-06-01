f5fda7fbcd9f95e5ad99eb6661b3897ce4bcc0db
Karlamon <zbv9522@aut.ac.nz>
2015-06-01 15:49:47 +1200

    Update loginprocess.php

--- loginprocess.php
+++ loginprocess.php
@@ -4,16 +4,14 @@
 <head>
 	<meta http-equiv="content-type" content="text/html; charset=utf-8" />
     <link rel="stylesheet" type="text/css" href="style.css">
-	<title>Submit new article - Article Searcher</title>
+	<title>Login - Article Searcher</title>
 </head>
 <body>
 	<h1>Article Searcher</h1>
-    <h2>Submit new article</h2>
+    <h2>Login</h2>
 	<p><?php
         $username = ($_GET["username"]);
         $password = ($_GET["password"]);
-        echo "<p>$username</p>";
-        echo "<p>$password</p>";
         $username_query = "SELECT username FROM accounts WHERE username = '$username'";
         $password_query = "SELECT password FROM accounts WHERE password = '$password'";
         if (($username == null) || ($password == null)) {
@@ -27,24 +25,20 @@
                        "<p>Error code ". mysqli_connect_errno(). ": ".
                        mysqli_connect_error(). "</p>".
                        "<p><a href=\"javascript:history.back(1);\">Try Again</a><br>
-                            <a href=\"index.php\">Return to Home Page</a></p>
-                        <hr>
-                        <div class = footer>
-                            Site created by Karl Smith (1390533).<br>
-                            <b>Email:</b> <a href=\"mailto:zbv9522@aut.ac.nz\">zbv9522@aut.ac.nz</a>
-                        </div>");
+                            <a href=\"index.php\">Return to Home Page</a></p>");
             $username_check = mysqli_query($dbConnect, $username_query);
             $password_check = mysqli_query($dbConnect, $password_query);
             if (mysqli_num_rows($username_check) > 0) {
                 if (mysqli_num_rows($password_check) > 0) {
-                    echo "<p>Login successful.</p>";
-                }
-                else {
-                    echo "<p>Incorrect username or password.</p>";
+                    header("location:submitarticleform.php");
                 }
             }
+            else {
+                echo "<p>Incorrect username or password.</p>
+                    <p><a href=\"javascript:history.back(1);\">Try Again</a>";
+            }
+            mysqli_close($dbConnect);
         }
-        mysqli_close($dbConnect);
 	?>
     <br><a href="index.php">Return to Home Page</a></p>
 </body>
