c1ec563ecfb416cd7bddc5f9935a1ebacc73a2d4
Karlamon <zbv9522@aut.ac.nz>
2015-06-02 17:01:09 +1200

    Update loginprocess.php

--- loginprocess.php
+++ loginprocess.php
@@ -29,7 +29,7 @@
             <p><a href=\"javascript:history.back(1);\">Try again</a>";
             }
             else {
-            //$dbConnect = @mysqli_connect("cmslamp14.aut.ac.nz", "(username)", "(password)", "(username)")
+//		$dbConnect = @mysqli_connect("cmslamp14.aut.ac.nz", "zbv9522", "litter7834", "zbv9522")
             $dbConnect = @mysqli_connect("localhost", "root", "", "zbv9522")
             or die("<p>Unable to connect to the database server.</p>".
             "<p>Error code ". mysqli_connect_errno(). ": ".
@@ -39,13 +39,17 @@
             $username_check = mysqli_query($dbConnect, $username_query);
             $password_check = mysqli_query($dbConnect, $password_query);
             if (mysqli_num_rows($username_check) > 0) {
-            if (mysqli_num_rows($password_check) > 0) {
-            header("location:submitarticleform.php");
-            }
+                if (mysqli_num_rows($password_check) > 0) {
+                    header("location:submitarticleform.php");
+                }
+                else {
+                    echo "<p>Incorrect username or password.</p>
+                    <p><a href=\"javascript:history.back(1);\">Try Again</a>";
+                }
             }
             else {
-            echo "<p>Incorrect username or password.</p>
-            <p><a href=\"javascript:history.back(1);\">Try Again</a>";
+                echo "<p>Incorrect username or password.</p>
+                <p><a href=\"javascript:history.back(1);\">Try Again</a>";
             }
             mysqli_close($dbConnect);
             }
