9a6de04bcf5117b5d2c90bef9eaa310e11331857
Karlamon <zbv9522@aut.ac.nz>
2015-06-03 22:24:39 +1200

    Update about.php

--- about.php
+++ about.php
@@ -1,10 +1,12 @@
+<!--Liang Chen (1392702)-->
+<!--Qingyang Guo (1395160)-->
 <!--Karl Smith (1390533)-->
 <!DOCTYPE html>
 <html>
     <head>
         <meta http-equiv="content-type" content="text/html; charset=utf-8" />
         <link rel="stylesheet" type="text/css" href="style.css">
-        <title>About this assignment - Article Searcher</title>
+        <title>Article Searcher System</title>
     </head>
     <body>
         <header>
@@ -13,17 +15,18 @@
     <bar>
         <div class="container">
             <div id="links">
-                <li><a href="index.php">Home Page</a></li>
-                <li class="selected"><a href="about.php">About this assignment</a></br></li>
+                <li class="selected"><a href="index.php">Home Page</a></br></li>
+                <li><a href="loginform.php">Submit new article</a><br></li>
+                <li><a href="searcharticleform.php">Search article</a><br></li>
+                <li><a href="createaccountform.php">Create account</a><br></li>
+                <li><a href="about.php">About</a></li>
             </div>
         </div>
     </bar>
     <div id="text">
-        <h2>About this assignment</h2>
-        <p>Assignment members:</p>
-        <li>Liang Chen (1392702) - <a href="mailto:cl19940413@hotmail.com">cl19940413@hotmail.com</a></li>
-        <li>Qingyang Guo (1395160) - <a href="mailto:g594xz@163.com">g594xz@163.com</a></li>
-        <li>Karl Smith (1390533) - <a href="mailto:zbv9522@aut.ac.nz">zbv9522@aut.ac.nz</a></li>
+        <h2>Welcome</h2>
+        <p>Welcome to Article Searcher. This program allows you to submit and search information on
+            various journals on the database.</p>
     </div>
 </body>
 </html>
