55ba04a3e7319b1d3d9c0ea4cf06e6602f3fb66a
Karlamon <zbv9522@aut.ac.nz>
2015-06-03 22:29:59 +1200

    Update loginprocess.php

--- loginprocess.php
+++ loginprocess.php
@@ -1,3 +1,5 @@
+<!--Liang Chen (1392702)-->
+<!--Qingyang Guo (1395160)-->
 <!--Karl Smith (1390533)-->
 <!DOCTYPE html>
 <html>
@@ -29,8 +31,7 @@
             <p><a href=\"javascript:history.back(1);\">Try again</a>";
             }
             else {
-//		$dbConnect = @mysqli_connect("cmslamp14.aut.ac.nz", "zbv9522", "litter7834", "zbv9522")
-            $dbConnect = @mysqli_connect("localhost", "root", "", "zbv9522")
+		$dbConnect = @mysqli_connect("cmslamp14.aut.ac.nz", "zbv9522", "", "zbv9522")
             or die("<p>Unable to connect to the database server.</p>".
             "<p>Error code ". mysqli_connect_errno(). ": ".
             mysqli_connect_error(). "</p>".
